fe2111e7bb398d6b69ab6cd92da722b2e982cd31
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-12 23:36:22 -0300

    Add files via upload
    Exercicio feito para treinar o uso dos operadores

---
+++ Exercicio 5.py
@@ -0,0 +1,6 @@
+a = int(input('Digite um número'))
+
+b = a * 2
+c = a * 3
+d = a ** (1/2)
+print ( 'O dobro de {} vale {}, o tripo de {} vale {}, e a raiz quadrade de {} é igua a {}'.format(a,b,a,c,a,d))
