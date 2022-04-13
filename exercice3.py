3a64794773553fcf16bc95a0e1a46305c92e891f
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-12 23:16:28 -0300

    Add files via upload
    Exercício criado para descobrir varias tipagens da variável digitada.

---
+++ exercice3.py
@@ -0,0 +1,12 @@
+valor = input('Digite algo:')
+print ('O tipo primitivo desse valor é',type(valor))
+print ('So tem espacos:', valor.isspace())
+print (' è um numero ', valor.isnumeric())
+print ('é alfabetico?', valor.isalpha())
+print (' é alfanumerico ?', valor.isalnum())
+print ('esta em maiuscula?', valor.isupper())
+print ('esta em minuscula?', valor .islower())
+print (' esta captalizada ?', valor.istitle())
+
+
+
