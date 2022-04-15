1cf4a74e955c09dfc2647b284d78145f7dacbc97
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-14 23:55:39 -0300

    Add files via upload

---
+++ exercicio 08.py
@@ -0,0 +1,16 @@
+a = int(input(' Uma distancia em metros '))
+print( 'A medida de {} correspomde a')
+
+b = a / 1000
+c = a / 100
+d = a / 10
+e = a * 10
+f = a * 100
+g = a * 1000
+
+print('{} km'.fomart('b'))
+print('{} hm'.foramt('c'))
+print( )
+print( )
+print( )
+
