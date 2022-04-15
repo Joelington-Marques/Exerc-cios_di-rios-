751d1954ac931066cfa1758e050279cabb5c0b72
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-14 23:59:25 -0300

    Add files via upload
    Exercicio criado para relizar conversao de medidas

--- exercicio 08.py
+++ exercicio 08.py
@@ -2,15 +2,13 @@ a = int(input(' Uma distancia em metros '))
 print( 'A medida de {} correspomde a')
 
 b = a / 1000
-c = a / 100
+c = a /1100
 d = a / 10
 e = a * 10
 f = a * 100
 g = a * 1000
 
-print('{} km'.fomart('b'))
-print('{} hm'.foramt('c'))
-print( )
-print( )
-print( )
+print('{} km'.format(b))
+print('{} hm'.format(c))
+
 
