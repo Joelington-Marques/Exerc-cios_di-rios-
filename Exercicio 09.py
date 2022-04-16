c9532416b0443caef96cafdf92ea9b0e7046e6bd
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-15 22:46:28 -0300

    Add files via upload
    Exercicio de tabuada

---
+++ Exercicio 09.py
@@ -0,0 +1,23 @@
+tabuada = int(input('Digite um numero para ver sua tabuada:'))
+tab1 = tabuada * 1
+tab2 = tabuada * 2
+tab3 = tabuada * 3
+tab4 = tabuada * 4
+tab5 = tabuada * 5
+tab6 = tabuada * 6
+tab7 = tabuada * 7
+tab8 = tabuada * 8
+tab9 = tabuada * 9
+tab10 = tabuada * 10
+print('----------')
+print('{} x  1 = {}'.format(tabuada,tab1))
+print('{} x  2 = {}'.format(tabuada,tab2))
+print('{} x  3 = {}'.format(tabuada,tab3))
+print('{} x  4 = {}'.format(tabuada,tab4))
+print('{} x  5 = {}'.format(tabuada,tab5))
+print('{} x  6 = {}'.format(tabuada,tab6))
+print('{} x  7 = {}'.format(tabuada,tab7))
+print('{} x  8 = {}'.format(tabuada,tab8))
+print('{} x  9 = {}'.format(tabuada,tab9))
+print('{} x 10 = {}'.format(tabuada,tab10))
+print('----------')
