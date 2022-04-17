9918d4ac1fb5e490bc6af3c93e800176f1fa4a44
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-17 11:08:02 -0300

    Add files via upload

---
+++ Exercice17.py
@@ -0,0 +1,7 @@
+import math
+oposto = float(input('Qual o comprimento do cateto oposto ? '))
+adjacente = float(input('Qual o comprimento do cateto adjacente ? '))
+
+hipotenusa = math.hypot(oposto,adjacente)
+
+print('A hipotenusa vai medir {:.2f}'.format(hipotenusa))
