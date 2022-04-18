51e504000345114084b6ca155f2934ab4965b1ad
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-18 08:14:38 -0300

    Add files via upload

--- exercice20.py
+++ exercice20.py
@@ -1,10 +1,11 @@
-import random
+from random import shuffle
 n1 = str(input('Primeiro aluno:'))
 n2 = str(input('Segundo aluno:'))
 n3 = str(input('Terceiro aluno'))
 n4 = str(input('Quarto aluno'))
 
 lista = [n1,n2,n3,n4]
-lista.sort()
+shuffle(lista)
 
-print('A ordem de apresentacao será:{}'.format(lista.sort()))
+print('A ordem de apresentacao será:')
+print(lista)
