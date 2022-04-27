0fe54bbc278c5766c0cb681a811695ccc0fb28a6
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-26 23:49:06 -0300

    Add files via upload

---
+++ exercice 28.py
@@ -0,0 +1,5 @@
+print('-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=')
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=')
+
+num = int(input('Em qual numero eu pensei '))
