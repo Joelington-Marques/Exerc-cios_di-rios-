e864c47e8d0bd00e3a42f4caa3b66a2313d0fa77
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-04-27 23:57:07 -0300

    Add files via upload

--- exercice 28.py
+++ exercice 28.py
@@ -1,5 +1,18 @@
+from random import randint
+from time import sleep
+adv = randint(0,5)
+
 print('-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=')
 print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
 print('-=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=')
 
-num = int(input('Em qual numero eu pensei '))
+num = int(input('Em qual numero eu pensei? '))
+
+
+print('PROCESSANDO...')
+sleep(3)
+if num == adv:
+    print('Você ganhou, parabéns...')
+
+else:
+    print('GANHEI! Eu pensei no número {} e não no {}'.format(adv,num))
