bf16df4a39f5eacdd3876af3ac5c28c73013ef99
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-05-12 23:57:10 -0300

    Add files via upload

--- Exercice 29.py
+++ Exercice 29.py
@@ -1,8 +1,9 @@
 vel = int(input('Qual é a velocidade atual do carro? '))
+multa = (vel - 80) * 7
 
 if vel <= 80:
     print('Tenha um bom dia! Dirija com segurança!')
 
 else:
     print('MULTADO! Voçê excedeu o limite permitido que é de 80Km/h')
-    print('Você deve pagar uma multa de R$280.00!')
+    print('Você deve pagar uma multa de R${}!'.format(multa))
