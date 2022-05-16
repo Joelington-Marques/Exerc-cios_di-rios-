ec3d3a247095cc22beef7be480283d1b0f5b7b75
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-05-16 06:34:14 -0300

    Add files via upload

--- exercice 35.py
+++ exercice 35.py
@@ -1,1 +1,12 @@
-cel = int 
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+print('Analisador de Triâgulos')
+print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+
+if a < b + c and b < c + a and c < a + b:
+    print('Os segmentos acima PODEM FORMAR um triângulo')
+else:
+    print('Os segmentos acima NÃO PODEM FORMA um triângulo')
