63b6c0f234d7e81fba38eb9b9a833a4aa28fc110
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-05-21 22:11:07 -0300

    Add files via upload

---
+++ Exercice 38.py
@@ -0,0 +1,7 @@
+a = int(input('Digite o primeiro numero '))
+b = int(input('Digite o segundo numero '))
+
+if a>b: print('{} é maior que {}'.format(a,b))
+elif a<b:print('{} é maior que {}'.format(b,a))
+else:
+    print('O dois numeros são iguais')
