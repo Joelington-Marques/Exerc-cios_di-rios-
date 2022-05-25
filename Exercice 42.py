f7aea99cac2f3ff1d3150db9c5e89b152e7ecddd
Joelington Marques Silva de Sousa <100744407+Joelington-Marques@users.noreply.github.com>
2022-05-25 10:30:36 -0300

    Add files via upload

--- Exercice 42.py
+++ Exercice 42.py
@@ -1,1 +1,17 @@
-zz
+
+a = float(input('Primeiro segmento: '))
+b = float(input('Segundo segmento: '))
+c = float(input('Terceiro segmento: '))
+
+if a < b + c and b < c + a and c < a + b:
+    if a == b == c:
+        print('Os segmentos formaram um triângulo Equilatero')
+    elif a != b and a != c and b != c:
+        print('Os segmentos formaram um triangulo Escaleno')
+    else:
+        print('Os segmentos formaram um triângulo Isoceles')
+
+else:
+    print('Os segmentos acima NÃO PODEM FORMA um triângulo')
+
+
